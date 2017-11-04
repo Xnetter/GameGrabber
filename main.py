61b6ea0642f35aef66f833cf7ff5fc4193ec54a7
Xnetter <ursingarrett@gmail.com>
2017-11-04 15:51:30 -0700

    Add main.py, the main file.

---
+++ main.py
@@ -0,0 +1,27 @@
+from Game import *
+from ServerContact import *
+
+searchToken = input("Please enter the name of the game you'd like to research: ")
+game = getGame(searchToken)
+initialize = input("Data will be compiled on {game}, is this correct? Y/N\n".format(game=game))
+if(initialize == 'Y' or initialize == 'y'):
+	pass
+else:
+	print("Here is a list of games with similar titles.")
+	#Currently the code only returns the first result of the list.
+	#TODO Cycle through the first 3 results then return 'Please try again' after all 3 have failed.
+	gameList = getGameList(searchToken)
+	count = 0
+	for title in gameList:
+		print("[{num}] {title}".format(num=count, title=title))
+		count+=1
+	gameIndex = int(input("Please enter the number of the title you wish to research, or -1 to escape: "))
+	if(gameIndex == -1):
+		exit()
+	game = gameList[gameIndex]
+print("Processing {game}\n".format(game = game))
+
+GameResearcher = find_game(game)
+#printGameInfo(GameResearcher.getGameName())
+#GameResearcher.rundown()
+#GameResearcher.listStats()
