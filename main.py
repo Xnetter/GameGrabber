6903a0f231a91546c2b62f6e188ba25e71d9939e
cody.j.b.scott@gmail.com <cody.j.b.scott@gmail.com>
2017-11-11 17:27:31 -0500

    Make if statement clearer

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ searchToken = input("Please enter the name of the game you'd like to research: "
 if(searchToken == "exit()"):
 	exit()
 game = getGame(searchToken)
+
 initialize = input("Data will be compiled on {game}, is this correct? [Y]/N\n".format(game=game))
-if(initialize == 'Y' or initialize == 'y'):
-	pass
-else:
+if initialize not in ('Yy'):
 	print("Here is a list of games with similar titles.")
 	#Currently the code only returns the first result of the list.
 	#TODO Cycle through the first 3 results then return 'Please try again' after all 3 have failed.
